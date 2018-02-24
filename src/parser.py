4d01d85cdeb2303c0f1f73c73e8266063ae4e8fe
jerr-chen <jerrychen31@gmail.com>
2018-02-23 22:23:56 -0600

    Parses CLI arguments. Basic arguments implemented.

---
+++ src/parser.py
@@ -0,0 +1,14 @@
+import argparse
+
+def get_parser():
+	parser = argparse.ArgumentParser(description='Configures "Twitter Writes the News" with parameters.')
+
+	parser.add_argument("--max_tweets", metavar='N', type=int, dest="max_tweets", default=500, help="The max tweets to collect for article construction.")
+	# timeout units = minutes
+	parser.add_argument("--timout", metavar='T', type=int, dest="timeout", default=5, help="The specified time before ending tweet collection.")
+	parser.add_argument("--keyword", dest="keyword", help="The keyword of collected tweets.")
+
+	return parser
+
+def get_args():
+	return get_parser().parse_args()
